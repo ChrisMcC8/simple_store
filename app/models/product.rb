d9573f93d4e54624070712bee28da28330a5eee5
ChrisMcC8 <chris.mccallum97@gmail.com>
2022-10-10 13:25:19 -0500

    added faker products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,3 @@
 class Product < ApplicationRecord
-    validates :title, presense: true
-    validates :price, presense: true
-    validates :stock_quantity, presense: true
+    validates :title, :price, :stock_quantity, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,12 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Product.create!([{
-    title: ,
-    price: ,
-    description: ,
-    stock_quantity: 
-}])
+require 'faker'
+
+676.times do
+    title = Faker::Coffee.blend_name
+    price = Faker::Commerce.price(0..100.0)
+    stock_quantity = Faker::Number.within(1..100)
+
+    Product.create(title: title, price: price, stock_quantity: stock_quantity)
+end
