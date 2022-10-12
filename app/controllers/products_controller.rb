047adf69c6688de8a778bffbc1df3530f32aa1f3
ChrisMcC8 <chris.mccallum97@gmail.com>
2022-10-12 13:47:37 -0500

    Added show action/view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,7 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+  def show # Within the products controller
+    @product = Product.find(params[:id]) # Find the product with the primary key mentioned in the URL.
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   get 'products/index'
+  get 'products/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
   get 'products' => 'products#index', as: 'products'
+
+  get 'products/:id' => 'products#show', as: 'product', id: /\d+/
 
   root to: 'products#index'
 end
